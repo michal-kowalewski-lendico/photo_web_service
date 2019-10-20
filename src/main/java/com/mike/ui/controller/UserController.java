800a08804c439d4e075bdd9b979c0bf9a8837c5f
Michal Kowalewski <mkowalewski83@gmail.com>
2019-10-20 18:57:12 +0200

    add support for xml

--- src/main/java/com/mike/ui/controller/UserController.java
+++ src/main/java/com/mike/ui/controller/UserController.java
@@ -6,6 +6,7 @@ import com.mike.ui.model.response.UserRest;
 import com.mike.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +16,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}",
+        produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserRest getUser(@PathVariable String id){
 
         UserDto userDto = userService.getUserByUserId(id);
@@ -26,7 +28,8 @@ public class UserController {
         return returnValue;
     }
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetailsRequestModel){
 
         UserDto userDto = new UserDto();
